c0a95fb1aef4b094bbce205da6a4ee462f1eaa17
jillmazzola <66700774+jillmazzola@users.noreply.github.com>
2020-07-29 23:42:44 -0400

    Added javascript function
    Added javascript function to enable hamburger menu.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 console.log("Hi there! Welcome to our site.")
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
